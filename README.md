f6f9beb7865cb6e2ca4217b70bf2e7116408b6ac
Boaz Poolman <boaz_skater@hotmail.com>
2021-03-19 19:04:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Strapi plugin config
+
+A quick description of config.
---
+++ controllers/config.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * config.js controller
+ *
+ * @description: A set of functions called "actions" of the `config` plugin.
+ */
+
+module.exports = {
+
+  /**
+   * Default action.
+   *
+   * @return {Object}
+   */
+
+  index: async (ctx) => {
+    // Add your own logic here.
+
+    // Send 200 `ok`
+    ctx.send({
+      message: 'ok'
+    });
+  }
+};
