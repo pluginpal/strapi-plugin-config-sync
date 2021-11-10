7609cf4ecffdbe468c8e12f6183600c119a90ac4
Boaz Poolman <boaz_skater@hotmail.com>
2021-11-10 16:42:44 +0100

    fix: Make CLI table wider

--- server/cli.js
+++ server/cli.js
@@ -14,7 +14,7 @@ const program = new Command();
 const initTable = (head) => {
   return new Table({
     head: [chalk.green('Name'), chalk.green(head || 'State')],
-    colWidths: [50, 15],
+    colWidths: [70, 15],
     chars: { top: '═',
       'top-mid': '╤',
       'top-left': '╔',
