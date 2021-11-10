68aa8b57e0dab80c777456b820ac4f41baeed3a3
Boaz Poolman <boaz_skater@hotmail.com>
2021-11-10 19:57:29 +0100

    chore: Grab plugin icon name from package.json

--- admin/src/components/PluginIcon/index.js
+++ admin/src/components/PluginIcon/index.js
@@ -8,6 +8,8 @@ import React from 'react';
 import { Icon } from '@strapi/design-system/Icon';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const PluginIcon = () => <Icon as={() => <FontAwesomeIcon icon="sync" />} width="16px" />;
+import pluginPkg from '../../../../package.json';
+
+const PluginIcon = () => <Icon as={() => <FontAwesomeIcon icon={pluginPkg.strapi.icon} />} width="16px" />;
 
 export default PluginIcon;
