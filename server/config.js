b4f20185e0ae1fcc62ebdfeccaea5fd4dad3425f
Luca Nerlich <5942270+LucaNerlich@users.noreply.github.com>
2023-03-01 11:56:26 +0100

    Add core-store.ee_information to default excludes
    Prevent licence codes from being written to the rep.

--- server/config.js
+++ server/config.js
@@ -12,6 +12,7 @@ module.exports = {
       "core-store.plugin_users-permissions_grant",
       "core-store.plugin_upload_metrics",
       "core-store.strapi_content_types_schema",
+      "core-store.ee_information",
     ],
   },
   validator() {},
