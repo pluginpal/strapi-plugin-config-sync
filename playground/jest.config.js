5ad9e8f0f6bcb30e07c07f3f44ee3489e473ac31
Boaz Poolman <boaz_skater@hotmail.com>
2024-05-08 21:45:04 +0200

    fix: add testing libraries to the playground

---
+++ playground/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'Integration test',
+  testMatch: ['**/__tests__/?(*.)+(spec|test).js'],
+  transform: {},
+  coverageDirectory: '../coverage/',
+  collectCoverage: true,
+};
