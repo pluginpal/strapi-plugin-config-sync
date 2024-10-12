e1aefbf5b6c35af33444b23ad135e81d7786cda3
Boaz Poolman <boaz_skater@hotmail.com>
2024-10-12 16:46:36 +0200

    chore: use yalc for playground development

--- playground/config/admin.js
+++ playground/config/admin.js
@@ -14,4 +14,7 @@ module.exports = ({ env }) => ({
     nps: env.bool('FLAG_NPS', true),
     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
   },
+  watchIgnoreFiles: [
+    '!**/.yalc/**/server/**',
+  ]
 });
