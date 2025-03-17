c51568fdde11f744e78d8092b91e3b9e46d3dc53
Boaz Poolman <boaz_skater@hotmail.com>
2025-03-17 11:31:21 +0100

    fix: correctly set the baseUrl for config-sync docs

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   url: 'https://docs.pluginpal.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/config-sync',
+  baseUrl: '/config-sync/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
